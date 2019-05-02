74edcc41a4885e5d8a63da2aac5b327d5ecb833a
ctghadiri <ctghadiri@gmail.com>
2019-05-02 00:14:07 -0700

    add modal to page and insert vaules

--- src/pages/admin/openApplications/index.js
+++ src/pages/admin/openApplications/index.js
@@ -19,6 +19,10 @@ class Admin extends Component {
       //   .then(res => console.log(res));
   }
 
+  approveApplication = id =>
+
+  closeApplication = id =>
+
 
     render() {
       return (
@@ -29,7 +33,8 @@ class Admin extends Component {
           <h1>Whats UP</h1>
           {this.state.truckData.map (truck => {
             return (
-              <h1>{truck.name}</h1>
+              <h1>{truck.businessName}</h1>
+              
             )
           })}
 
@@ -39,3 +44,45 @@ class Admin extends Component {
   };
   
   export default Admin;
+
+
+
+  // <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+  Launch demo modal
+</button>
+
+// <!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">{truck.businessName}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        {this.truck.businessName}
+        {this.truck.website}
+        {this.truck.cuisine}
+        {this.truck.menu}
+        {this.truck.firstName}
+        {this.truck.middleInitial}
+        {this.truck.lastName}
+        {this.truck.email}
+        {this.truck.phone}
+        {this.truck.address}
+        {this.truck.address2}
+        {this.truck.city}
+        {this.truck.state}
+        {this.truck.zip}
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary approve">Approve</button>
+        <button type="button" class="btn btn-primary close">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
